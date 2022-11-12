88636674bf56a09a14384aa61a5d4ad43c76dfa0
anesnov <61014956+aleteklas@users.noreply.github.com>
2022-11-13 01:44:23 +0300

    Добавьте файлы проекта.

---
+++ Graph/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Graph
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.move = new System.Windows.Forms.RadioButton();
+            this.remove = new System.Windows.Forms.RadioButton();
+            this.AddE = new System.Windows.Forms.RadioButton();
+            this.AddV = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sToolStripMenuItem = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutCreatorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(0, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(240, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add vertex";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(531, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.move);
+            this.groupBox1.Controls.Add(this.remove);
+            this.groupBox1.Controls.Add(this.AddE);
+            this.groupBox1.Controls.Add(this.AddV);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(534, 34);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(240, 404);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // move
+            // 
+            this.move.Appearance = System.Windows.Forms.Appearance.Button;
+            this.move.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.move.Location = new System.Drawing.Point(6, 183);
+            this.move.Name = "move";
+            this.move.Size = new System.Drawing.Size(228, 35);
+            this.move.TabIndex = 4;
+            this.move.TabStop = true;
+            this.move.Text = "Move";
+            this.move.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.move.UseVisualStyleBackColor = true;
+            // 
+            // remove
+            // 
+            this.remove.Appearance = System.Windows.Forms.Appearance.Button;
+            this.remove.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.remove.Location = new System.Drawing.Point(6, 233);
+            this.remove.Name = "remove";
+            this.remove.Size = new System.Drawing.Size(228, 35);
+            this.remove.TabIndex = 3;
+            this.remove.TabStop = true;
+            this.remove.Text = "Remove Element";
+            this.remove.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.remove.UseVisualStyleBackColor = true;
+            // 
+            // AddE
+            // 
+            this.AddE.Appearance = System.Windows.Forms.Appearance.Button;
+            this.AddE.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AddE.Location = new System.Drawing.Point(6, 133);
+            this.AddE.Name = "AddE";
+            this.AddE.Size = new System.Drawing.Size(228, 35);
+            this.AddE.TabIndex = 2;
+            this.AddE.TabStop = true;
+            this.AddE.Text = "Add Edge";
+            this.AddE.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.AddE.UseVisualStyleBackColor = true;
+            // 
+            // AddV
+            // 
+            this.AddV.Appearance = System.Windows.Forms.Appearance.Button;
+            this.AddV.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AddV.Location = new System.Drawing.Point(6, 82);
+            this.AddV.Name = "AddV";
+            this.AddV.Size = new System.Drawing.Size(228, 35);
+            this.AddV.TabIndex = 1;
+            this.AddV.TabStop = true;
+            this.AddV.Text = "Add Vertex";
+            this.AddV.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.AddV.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(3, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(525, 411);
+            this.panel1.TabIndex = 4;
+            this.panel1.Visible = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.loadToolStripMenuItem,
+            this.sToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            // 
+            // sToolStripMenuItem
+            // 
+            this.sToolStripMenuItem.Name = "sToolStripMenuItem";
+            this.sToolStripMenuItem.Size = new System.Drawing.Size(97, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem,
+            this.aboutCreatorToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutCreatorToolStripMenuItem
+            // 
+            this.aboutCreatorToolStripMenuItem.Name = "aboutCreatorToolStripMenuItem";
+            this.aboutCreatorToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
+            this.aboutCreatorToolStripMenuItem.Text = "About creator";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Graph Tool";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.groupBox1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator sToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutCreatorToolStripMenuItem;
+        private System.Windows.Forms.RadioButton move;
+        private System.Windows.Forms.RadioButton remove;
+        private System.Windows.Forms.RadioButton AddE;
+        private System.Windows.Forms.RadioButton AddV;
+    }
+}
+
---
+++ Graph/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graph
+{    
+    public partial class Form1 : Form
+    {
+
+        GGraph graph;
+        Tuple<int, int> clicked = null;
+        int current = -1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            graph = new GGraph(new List<Tuple<int, int>>(), new List<Tuple<int, int, int, int>>());
+            SetStyle(ControlStyles.UserPaint, true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.DoubleBuffer, true);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            
+            g.DrawRectangle(graph.np, panel1.Location.X, panel1.Location.Y, panel1.Width+panel1.Location.X, panel1.Height);            
+            
+            if (graph.edges != null)
+            {
+                for (int i = 0; i < graph.edges.Count; i++)
+                {
+                    graph.edges[i].Deconstruct<int, int, int, int>(out int x1, out int y1, out int x2, out int y2);
+                    g.DrawLine(graph.np, x1+graph.size/2, y1 + graph.size / 2, x2 + graph.size / 2, y2 + graph.size / 2);
+                }
+            }
+            if (graph.vertexes != null)
+            {
+                for (int i = 0; i < graph.vertexes.Count; i++)
+                {
+                    graph.vertexes[i].Deconstruct<int, int>(out int x, out int y);
+                    Rectangle rect = new Rectangle(x, y, graph.size, graph.size);
+
+                    g.DrawEllipse(graph.np, rect);
+                    g.FillEllipse(graph.nb, rect);
+                    g.DrawString((i + 1).ToString(), graph.drawFont, graph.fontb, new Point(x, y));
+                }
+            }
+            if (clicked != null)
+            {
+                clicked.Deconstruct<int, int>(out int x, out int y);
+                Rectangle rect = new Rectangle(x, y, graph.size, graph.size);
+
+                Brush sb = new SolidBrush(Color.Crimson);
+                g.DrawEllipse(graph.np, rect);
+                g.FillEllipse(sb, rect);
+                g.DrawString((graph.vertexes.IndexOf(clicked) + 1).ToString(), graph.drawFont, sb, new Point(x, y));
+            }
+        }        
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            graph.vertexes.Add(new Tuple<int, int>(rnd.Next(0, 480), rnd.Next(0, 380)));
+            this.Refresh();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (AddE.Checked)
+            {
+                foreach (Tuple<int, int> vertex in graph.vertexes)
+                {
+                    if (Math.Pow(vertex.Item1 - e.X, 2) + Math.Pow(vertex.Item2 - e.Y, 2) < graph.size * graph.size)
+                    {
+                        if (clicked == null)
+                        {
+                            clicked = vertex;
+                            break;
+                        }
+                        else
+                        {
+                            foreach (Tuple<int, int> nvertex in graph.vertexes)
+                            {
+                                if (clicked != nvertex && Math.Pow(nvertex.Item1 - e.X, 2) + Math.Pow(nvertex.Item2 - e.Y, 2) < graph.size * graph.size)
+                                {
+                                    graph.edges.Add(new Tuple<int, int, int, int>(clicked.Item1, clicked.Item2, nvertex.Item1, nvertex.Item2));
+                                    clicked = null;
+                                    break;
+                                }
+                            }
+                            break;
+                        }
+                    }
+                }
+
+                this.Refresh();
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (move.Checked)
+            {
+                if (current == -1)
+                {
+                    foreach (Tuple<int, int> vertex in graph.vertexes)
+                    {
+                        if (Math.Pow(vertex.Item1 - e.X, 2) + Math.Pow(vertex.Item2 - e.Y, 2) < graph.size * graph.size)
+                        {
+                            current = graph.vertexes.IndexOf(vertex);
+                            label1.Text = current.ToString();
+                            break;
+                        }
+                    }
+                }
+                this.Refresh();
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (move.Checked)
+            {
+                current = -1;
+                label1.Text = current.ToString();
+                this.Refresh();
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (move.Checked)
+            {
+                if (current != -1)
+                {
+                    graph.vertexes[current] = new Tuple<int, int>(e.X, e.Y);
+                    this.Refresh();
+                }
+            }
+        }
+    }
+
+    class GGraph
+    {
+        public List<Tuple<int, int>> vertexes = new List<Tuple<int, int>>();
+        public List<Tuple<int, int, int, int>> edges = new List<Tuple<int, int, int, int>>();
+        public Pen np;
+        public Brush nb;
+        public Font drawFont;
+        public Brush fontb;
+        public int size;
+
+        public GGraph(List<Tuple<int, int>> vertexes, List<Tuple<int, int, int, int>> edges)
+        {
+            this.vertexes = vertexes;
+            this.edges = edges;
+
+            np = new Pen(Color.Black, 2);
+            nb = new SolidBrush(Color.Wheat);
+            fontb = new SolidBrush(Color.Black);
+            drawFont = new Font("Arial", 12);
+            size = 20;
+        }
+
+    }
+}
